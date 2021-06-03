1756be0cab8a003288258bdd80bdce1d99e36315
maofansa <43571772+maofansa@users.noreply.github.com>
2021-06-03 09:25:10 +0800

    Connect to database (MySQL)

---
+++ database/mysql.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+var SqlDB *sql.DB
+
+func init() {
+	var err error
+	// 172.17.0.2:3306
+	SqlDB, err = sql.Open("mysql", "root:root@tcp(172.17.0.2:3306)/jobsubmission?charset=utf8")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	err = SqlDB.Ping()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
